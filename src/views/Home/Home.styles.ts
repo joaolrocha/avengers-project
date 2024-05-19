8e8569813d74a7f6f778f36aab576769e110b73f
joaolrocha <joaolrocha07@gmail.com>
2024-05-18 21:59:34 -0300

    feat: add carrousel and redarrow icon

--- src/views/Home/Home.styles.ts
+++ src/views/Home/Home.styles.ts
@@ -3,4 +3,34 @@ import { styled } from "styled-components";
 export const HomeContainer = styled.div`
 width: 100%;
 display: flex;
+align-items: center;
+justify-content: center;
+gap: 3rem;
+
+padding-top: 5rem;
 `
+
+export const CarouselWrapper = styled.div`
+  width: 90%;
+  .slick-slide {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .slick-prev, .slick-next {
+    z-index: 1;
+  }
+  .slick-prev:before, .slick-next:before {
+   content: none;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const CustomArrow = styled.div`
+  width: 30px;
+  height: 30px;
+  background-size: contain;
+  background-repeat: no-repeat;
+`;
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,27 +1,79 @@
 // src/views/Home/Home.tsx
 import React from 'react';
 import Layout from '../../components/common/Layout/Layout';
-import MainCard from '../../components/common/MainCard/MainCard';
 import useMockHeroes from '../../hooks/useMockHeroes';
-import { HomeContainer } from './Home.styles';
+import MainCard from '../../components/common/MainCard/MainCard';
+import Slider from 'react-slick';
+import { HomeContainer, CarouselWrapper, CustomArrow } from './Home.styles';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import redArrow from '../../assets/images/redArrow.svg';
+
+const NextArrow = (props: any) => {
+  const { className, style, onClick } = props;
+  return (
+    <CustomArrow
+      className={className}
+      style={{ ...style, display: 'block', backgroundImage: `url(${redArrow})` }}
+      onClick={onClick}
+    />
+  );
+};
+
+const PrevArrow = (props: any) => {
+  const { className, style, onClick } = props;
+  return (
+    <CustomArrow
+      className={className}
+      style={{ ...style, display: 'block', backgroundImage: `url(${redArrow})`, transform: 'rotate(180deg)' }}
+      onClick={onClick}
+    />
+  );
+};
 
 const Home: React.FC = () => {
   const heroes = useMockHeroes();
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
   return (
     <Layout>
       <HomeContainer>
-        {heroes.map(hero => (
-          <MainCard
-            key={hero.id}
-            id={hero.id}
-            image={hero.image}
-            name={hero.name}
-            description={hero.description}
-          />
-        ))}
+        <CarouselWrapper>
+          <Slider {...settings}>
+            {heroes.map(hero => (
+              <MainCard
+                key={hero.id}
+                id={hero.id}
+                image={hero.image}
+                name={hero.name}
+                description={hero.description}
+              />
+            ))}
+          </Slider>
+        </CarouselWrapper>
       </HomeContainer>
     </Layout>
   );
 };
+
 export default Home;
